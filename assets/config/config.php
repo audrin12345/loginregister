7b3d02e7312381d844b3fe764b37db49b3dd7d63
audrin12345 <31085962+audrin12345@users.noreply.github.com>
2021-06-21 15:49:33 +0800

    Update config.php

--- assets/config/config.php
+++ assets/config/config.php
@@ -1,1 +1,17 @@
 
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+// this is the database name in xampp
+define('DB_NAME', 'loginexam');
+ 
+// connect to MySQL database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
