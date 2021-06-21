03cb2f2d652f8a88477ca23d56d08bd1adc56015
audrin12345 <31085962+audrin12345@users.noreply.github.com>
2021-06-21 15:41:22 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/scss/index.css">
+</head>
+
+<body>
+    <section class="welcome">
+        <h1 class="my-5 welcome__title">Hi,Welcome to
+            GUL.PH.</h1>
+        <div class="welcome__button">
+            <a href="login__form.php" class="btn btn-warning">LOG IN</a>
+            <a href="register__form.php" class="btn btn-danger ml-3">SIGN UP</a>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ register__form.php
@@ -0,0 +1,38 @@
+<?php include 'assets/config/register.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/scss/index.css">
+</head>
+<body>
+    <div class="wrapper register">
+        <h2 class="register__title">Sign Up</h2>
+        <p class="register__desc">Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a class="register__signup" href="login__form.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
